839cc7e19b35bcd3d6e1b1d31a2c2a05b96de805
aucguy <null>
2024-04-13 19:59:41 -0500

    Add zoom and panning

--- constants.gd
+++ constants.gd
@@ -4,3 +4,7 @@ const maximum_mana = 100
 const SHOOTER_COST = 10
 const DARK_SPREAD_INTERVAL = 1
 const MAXIMUM_HEALTH = 100
+const MOVE_SPEED = 300
+const ZOOM_SPEED = 2
+const ZOOM_MAX = 2
+const ZOOM_MIN = 0.25
--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CanvasLayer
 
 const constants = preload("res://constants.gd")
 
